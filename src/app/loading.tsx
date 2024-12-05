5c1a16e2860e6c8ae0d5b6d68dd790cad0c1cf2f
Majharul Tanvir <majharul5456@gmail.com>
2024-12-05 22:10:24 +0600

    [Completed] Loading and not-found setup

---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading() {
+  return <div className="min-h-screen justify-center items-center">
+    <p className="text-green-800 dark:text-green-400">Loading...</p>
+  </div>;
+}
